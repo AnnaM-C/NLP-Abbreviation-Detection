b4119e7b132d6bd2cd9b250c83e32ef29daf15b4
Evan99w <50713644+Evan99w@users.noreply.github.com>
2024-05-22 15:56:30 +0100

    fix(flask): add error handling to predict endpoint add error display to frontend

--- flask_main.py
+++ flask_main.py
@@ -1,7 +1,14 @@
-from flask import Flask, request, render_template, jsonify
-from datetime import datetime
-from predictor import Predictor
 import csv
+from datetime import datetime
+import logging
+
+from flask import Flask, request, render_template, jsonify
+
+from predictor import Predictor
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__, template_folder='templates')
 
@@ -18,12 +25,25 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     cur_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    input_sentence = request.json['input']
-    prediction_obj = predictor.predict(input_sentence)
-    if prediction_obj == "":
-        with open("monitor_log.csv", mode='a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow([input_sentence, "", "", cur_time])
+
+    data = request.get_json()
+
+    if not data or 'input' not in data:
+        logger.error("No sentence provided in the request data.")
+        return jsonify({"error": "No sentence provided"}), 422
+
+    input_sentence = data['input'].strip()
+
+    try:
+        prediction_obj = predictor.predict(input_sentence)
+    except ValueError as e:
+        logger.error(f"An error occurred while predicting the sentence: {str(e)}")
+        return jsonify({"error": str(e)}), 422
+    except Exception as e:
+        logger.error(f"An unexpected error occurred while predicting the sentence: {str(e)}")
+        return jsonify({"error": "An unexpected error occurred."}), 400
+    
+    logger.info("Prediction successful")
 
     with open("monitor_log.csv", mode='a', newline='') as file:
         writer = csv.writer(file)
